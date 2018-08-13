f11c5ddc265f213016d27d659a3986b3ad4d7818
Jason Kung <jason.kung@ecoluminatech.com>
2018-08-13 16:44:17 +0800

    onvif GetStreamUri

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from WSDiscovery import WSDiscovery
 from urlparse import urlparse, parse_qs
 #from urllib.parse import urlparse, parse_qs
 
+from onvif import ONVIFCamera
+
 def Onvifdiscovery(retries=3):
     wsd = WSDiscovery()
     wsd.start()
@@ -25,10 +27,27 @@ def Onvifdiscovery(retries=3):
             selected = urlparse(camera)
             ipclist.append(selected.netloc)
 
-    return ipclist
+    return resp
 
 
 if __name__ == "__main__":
-    print Onvifdiscovery()
+    IPC = []
+    token = ''
+    IPC = Onvifdiscovery()
 
+    if len(IPC) < 1:
+        pass
+    else:
+        #print IPC
+        for camera in IPC:
+            selected = urlparse(camera)
+            try:
+                mycam = ONVIFCamera(selected.netloc, 80, 'user', '12345678')
+            except:
+                continue
+            media_service = mycam.create_media_service()
+            profiles = media_service.GetProfiles()
+            token = profiles[0]._token
+            uri = media_service.GetStreamUri()
 
+            print (uri.Uri).replace("rtsp://", "rtsp://" + "user" + ":" + "12345678" + "@")
